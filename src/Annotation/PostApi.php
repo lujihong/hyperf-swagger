87038231f567cef19d83401c6e19187b05458351
lujihong <382217590@qq.com>
2021-01-06 12:42:16 +0800

    初始化

---
+++ src/Annotation/PostApi.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Apidog\Annotation;
+
+use Hyperf\HttpServer\Annotation\Mapping;
+
+/**
+ * @Annotation
+ * @Target({"METHOD"})
+ */
+class PostApi extends Mapping
+{
+    public $path;
+
+    public $summary;
+
+    public $description;
+
+    public $deprecated;
+
+    public $methods = ['POST'];
+
+    public function __construct($value = null)
+    {
+        parent::__construct($value);
+        if (is_array($value)) {
+            foreach ($value as $key => $val) {
+                if (property_exists($this, $key)) {
+                    $this->{$key} = $val;
+                }
+            }
+        }
+    }
+}
