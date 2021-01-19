6dada5341b97d4ce85983c197c9cb91cf12be4c7
lujihong <382217590@qq.com>
2021-01-19 11:45:12 +0800

    增加自定义phone手机号码验证规则

--- src/Validation/ValidationCustomRule.php
+++ src/Validation/ValidationCustomRule.php
@@ -17,6 +17,14 @@ class ValidationCustomRule
         return '自定义错误';
     }
 
+    public function phone($attribute, $value)
+    {
+        if (! preg_match('/^1[3456789]{1}\d{9}$/', trim($value))) {
+            return '手机号不合法';
+        }
+        return true;
+    }
+
     public function crontab($attribute, $value)
     {
         if (! preg_match('/^((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)$/i', trim($value))) {
