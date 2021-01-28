f63ea47f8bd08490982a5365386441f6c8d0dfaf
lujihong <382217590@qq.com>
2021-01-28 14:57:57 +0800

    新增自定义身份证号码验证规则

--- src/Validation/ValidationCustomRule.php
+++ src/Validation/ValidationCustomRule.php
@@ -7,6 +7,7 @@ namespace Hyperf\Apidoc\Validation;
 class ValidationCustomRule
 {
     /**
+     * 手机号码验证
      * @param $attribute 属性
      * @param $value 属性值
      * @return bool|string 校验错误则返回错误信息, 正确则返回 true
@@ -18,6 +19,62 @@ class ValidationCustomRule
         }
 
         return true;
+    }
+
+    /**
+     * 身份证号码验证
+     * @param $id
+     * @return bool
+     */
+    public function identity_card($id)
+    {
+        $id = strtoupper($id);
+        $regx = "/(^\d{15}$)|(^\d{17}([0-9]|X)$)/";
+        $arr_split = [];
+        $errorMsg = '身份证号码不合法';
+        if (!preg_match($regx, $id)) {
+            return $errorMsg;
+        }
+
+        //检查15位
+        if (15 == strlen($id)) {
+            $regx = "/^(\d{6})+(\d{2})+(\d{2})+(\d{2})+(\d{3})$/";
+            @preg_match($regx, $id, $arr_split);
+            //检查生日日期是否正确
+            $dtm_birth = "19" . $arr_split[2] . '/' . $arr_split[3] . '/' . $arr_split[4];
+            if (!strtotime($dtm_birth)) {
+                return $errorMsg;
+            } else {
+                return true;
+            }
+        } else {
+            //检查18位
+            $regx = "/^(\d{6})+(\d{4})+(\d{2})+(\d{2})+(\d{3})([0-9]|X)$/";
+            @preg_match($regx, $id, $arr_split);
+            $dtm_birth = $arr_split[2] . '/' . $arr_split[3] . '/' . $arr_split[4];
+            //检查生日日期是否正确
+            if (!strtotime($dtm_birth)) {
+                return $errorMsg;
+            } else {
+                //检验18位身份证的校验码是否正确。
+                //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
+                $arr_int = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+                $arr_ch = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+                $sign = 0;
+                for ($i = 0; $i < 17; $i++) {
+                    $b = (int)$id[$i];
+                    $w = $arr_int[$i];
+                    $sign += $b * $w;
+                }
+                $n = $sign % 11;
+                $val_num = $arr_ch[$n];
+                if ($val_num != substr($id, 17, 1)) {
+                    return $errorMsg;
+                } else {
+                    return true;
+                }
+            }
+        }
     }
 
     public function crontab($attribute, $value)
